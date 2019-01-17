9a99cbdf7bec03494e72f39f217484fd021ff9ab
singer30 <lukas.w.singer@gmail.com>
2019-01-16 21:21:06 -0800

    Implement dynamic video player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,44 @@
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
+import VideoPlayer from './VideoPlayer.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allVideos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  changeCurrentVideo(event) {
+    console.log(event);
+    this.setState({
+      currentVideo: exampleVideoData[event.target.id]
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.allVideos} changeCurrentVideo={this.changeCurrentVideo.bind(this)}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,8 +2,8 @@ import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
-    {props.videos.map(video => 
-      <VideoListEntry video={video} />
+    {props.videos.map((video, i) => 
+      <VideoListEntry video={video} index={i} changeCurrentVideo={props.changeCurrentVideo} />
     )}
   </div>
 );
